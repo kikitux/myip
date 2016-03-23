2e31defd77e238dd38d2eb0ba3fde448d9ee504e
Alvaro Miranda <kikitux@gmail.com>
2016-03-24 00:38:29 +1300

    update builtin

--- builtin/builtin_myip.c
+++ builtin/builtin_myip.c
@@ -26,11 +26,12 @@ myip (list) WORD_LIST *list;
   WORD_LIST *w = NULL;
   int opt = 0;
   char *var = NULL;
+  char *iface = NULL;
   bool overwrite = false;
   bool ipv6 = false;
 
   reset_internal_getopt();
-  while ((opt = internal_getopt (list, "ov:h6")) != -1){
+  while ((opt = internal_getopt (list, "ov:h6i:")) != -1){
     switch(opt) {
       case 'o':
         overwrite = true;
@@ -40,6 +41,9 @@ myip (list) WORD_LIST *list;
         break;
       case 'v':
         var = list_optarg; /* should add check for valid variable name */
+        break;
+      case 'i':
+        iface = list_optarg; /* should add check for valid variable name */
         break;
       case 'h':
         builtin_usage();
@@ -62,32 +66,51 @@ myip (list) WORD_LIST *list;
       printf("OS var %s has values. not overwriting\n",var);
     }
     if (!v || overwrite == true) {
-      printf("setting values\n");
       if (ipv6 == true) {
-        addr6 = myexip6();
-        inet_ntop(AF_INET6, &(addr6), str6, INET6_ADDRSTRLEN);
-        bind_variable (var, str6, 0);
+        if ( iface == NULL ) {
+          addr6 = myexip6();
+          inet_ntop(AF_INET6, &(addr6), str6, INET6_ADDRSTRLEN);
+          bind_variable(var, str6, 0);
+        } else {
+          addr6 = mylocalip6(iface);
+          inet_ntop(AF_INET6, &(addr6), str6, INET6_ADDRSTRLEN);
+          bind_variable(var, str6, 0);
+        }
       } else {
-        addr = myexip();
-        inet_ntop(AF_INET, &(addr), str, INET_ADDRSTRLEN);
-        bind_variable (var, str, 0);
+        if ( iface == NULL ) {
+          addr = myexip();
+          inet_ntop(AF_INET, &(addr), str, INET_ADDRSTRLEN);
+          bind_variable (var, str, 0);
+        } else {
+          addr = mylocalip(iface);
+          inet_ntop(AF_INET, &(addr), str, INET_ADDRSTRLEN);
+          bind_variable (var, str, 0);
+        }
       }
     }
   } else {
-      printf("printing values\n");
       if (ipv6 == true) {
+        if ( iface == NULL ) {
           addr6 = myexip6();
           print_in6_addr(addr6);
+        } else {
+          addr6 = mylocalip6(iface);
+          print_in6_addr(addr6);
+        }
       } else {
+        if ( iface == NULL ) {
           addr = myexip();
           print_in_addr(addr);
+        } else {
+          addr = mylocalip(iface);
+          print_in_addr(addr);
+        }
       }
   }
 
   if (list != 0){
     w = list;
     while (w != 0){
-      //printf("%s\n", w->word->word);
       w = w->next;
     }
   }
@@ -100,15 +123,15 @@ char *builtin_myip_doc[] = {
 	"the service is done by myexternalip.com\n",
 	"usage:\n",
 	"myip -v var ipv4 [-o]\n",
-	"myip -v var ipv6 [-o]\n",
+	"myip -v var ipv6 -6 [-o]\n",
 	(char *)NULL
 };
 
 struct builtin builtin_myip_struct = {
-	"builtin_myip",			/* builtin name */
+	"myip",			/* builtin name */
 	myip,			/* function implementing the builtin */
 	BUILTIN_ENABLED,		/* initial flags for builtin */
 	builtin_myip_doc,			/* array of long documentation strings. */
-	"myip -v var -o ipv4",	/* usage synopsis; becomes short_doc */
+	"myip -v var -o",	/* usage synopsis; becomes short_doc */
 	0				/* reserved for internal use */
 };
